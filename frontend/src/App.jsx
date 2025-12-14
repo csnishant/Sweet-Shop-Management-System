a016afdb0450ce9636eec52118beee3e2bc5b0dc
Nishant Choudhary <cs.nishantchoudhary@gmail.com>
2025-12-14 13:11:15 +0530

    implement sweet fetching, searching, and purchasing functionalities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import {
 import Login from "./auth/Login.jsx";
 import Register from "./auth/Register.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
+import AdminDashboard from "./pages/AdminDashboard.jsx";
 
 function App() {
   const token = localStorage.getItem("token");
@@ -20,6 +21,10 @@ function App() {
         <Route
           path="/"
           element={token ? <Dashboard /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/admin"
+          element={token ? <AdminDashboard/> : <Navigate to="/login" />}
         />
       </Routes>
     </Router>
